aa5a81c6e0063eb3adeb9eecbbf6c03f043db24d
Vinícius Godoy <111363060+VnsGodoy@users.noreply.github.com>
2022-10-21 09:22:42 -0300

    Update Calculadora.asm

--- Calculadora.asm
+++ Calculadora.asm
@@ -9,54 +9,68 @@ Baixo DB '   _________________________________________________________________ $
 MSG1 DB ' -> Digite o primeiro numero: $'
 MSG2 DB ' -> Digite o segundo numero: $'
 MSG3 DB ' -> Digite o sinal que deseja: $'
-SOMA DB ?,'$'
-SUBTRACAO DB ?,'$'
+OP1 DB ?,'$'
+OP2 DB ?,'$'
+
 .CODE
- 
+
+PULA_LINHA PROC
+
+MOV DL, 10  ;Pula linha
+MOV AH, 02
+INT 21H
+INT 21H
+
+PULA_LINHA ENDP
+
+
+SOMA PROC
+
+MOV BL, OP1
+SUB BL, 30H
+MOV BH, OP2
+SUB BH, 30H
+ADD BL, BH
+SUB BL, 30H
+RET
+
+
+SOMA ENDp
+
+
+
+
+SUBI PROC
+
+SUBI ENDP
+
 MAIN PROC
-MOV AX,@DATA
+MOV AX,@DATA  ;Começa o Programa
 MOV DS, AX
  
 LEA DX, Cima ;Imprime uma mensagem
 MOV AH, 09
 INT 21H
  
-MOV DL, 10  ;Pula linha
-MOV AH, 02
-INT 21H
+CALL PULA_LINHA
  
 LEA DX, Começo
 MOV AH, 09
 INT 21H
  
-MOV DL, 10
-MOV AH, 02
-INT 21H
- 
-MOV DL, 10
-MOV AH, 02
-INT 21H
+CALL PULA_LINHA
  
 LEA DX, BV
 MOV AH, 09
 INT 21H
  
-MOV DL, 10
-MOV AH, 02
-INT 21H
+CALL PULA_LINHA
  
 LEA DX, Baixo
 MOV AH, 09
 INT 21H
  
-MOV DL, 10
-MOV AH, 02
-INT 21H
- 
-MOV DL, 10
-MOV AH, 02
-INT 21H
- 
+CALL PULA_LINHA
  
 LEA DX, MSG1
 MOV AH, 09
@@ -65,12 +79,9 @@ INT 21H
 MOV AH, 01     ;Primeira variável
 INT 21H
  
-SUB AL, 30H  ; Transformação Tabela Asc
- 
-MOV DL, 10
-MOV AH, 02
- 
-INT 21H
+MOV OP1, AL  ; Manda para a variável
+
+CALL PULA_LINHA
  
 LEA DX, MSG2    
 MOV AH, 09      
@@ -79,13 +90,9 @@ INT 21H
 MOV AH, 01   ;Segunda Variável
 INT 21H
  
-MOV AL, 30H ; Transformação Tabela Asc
-MOV BH, AL
+MOV OP2, AL
  
-MOV DL, 10
-MOV AH, 02
- 
-INT 21H
+CALL PULA_LINHA
  
 LEA DX, MSG3  
 MOV AH, 09      
@@ -95,31 +102,19 @@ MOV AH, 01
 INT 21H
  
 CMP AL, '+'
-JA Operador
- 
-Operador:
-ADD BL, 0
-ADD BH, 0
-ADD BL, BH
+JE SOMAOP
+
+SOMAOP:
+CALL SOMA
 MOV DL, BL
-ADD DL, 48
-INT 21H
- 
-MOV AL, DL
 MOV AH, 02
+INT 21H
+JMP FIM
+
  
- 
- 
+FIM:
+
 MOV AH, 4CH
 INT 21H
 MAIN ENDP
 END MAIN
- 
- 
- 
- 
- 
- 
- 
- 
-
